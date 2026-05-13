8e078d40dace0c8658ab7be5b9c193ad021bcd25
gadflysu <git@gadflysu.com>
2026-05-13 08:04:17 +0000

    feat: bump Formula to v0.3.2

--- Formula/aps.rb
+++ Formula/aps.rb
@@ -1,26 +1,26 @@
 class Aps < Formula
   desc "Interactive session picker for Claude Code and Opencode"
   homepage "https://github.com/gadflysu/aps"
-  version "0.3.1"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gadflysu/aps/releases/download/v#{version}/aps_darwin_arm64.tar.gz"
-      sha256 "10ee1521f5063157290fcac53fa5dd60460250fbeec1ec1a6885adc20bb6a900"
+      sha256 "00c970c8dfb9a0972e1b5b2c7bd6c303e1ee1ad2765b9649e87859738886600b"
     else
       url "https://github.com/gadflysu/aps/releases/download/v#{version}/aps_darwin_amd64.tar.gz"
-      sha256 "ced0fd053167ae62fe86bf40f789c30a93b8ea6888eee1a2b420847d812583d2"
+      sha256 "0b731adbfb8fadfb228b5d4e14ffe86abbafc0350d7d2f9ff3b9b93fc15ace37"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/gadflysu/aps/releases/download/v#{version}/aps_linux_arm64.tar.gz"
-      sha256 "30638c8aadfa5e29c0034cdd025df2009c68594e770fe79b17c9de29bf809dea"
+      sha256 "5f3eb71a916ad1b592bb3d36a75ac013dee7d10b1462369eb40b12cce9f6c0f3"
     else
       url "https://github.com/gadflysu/aps/releases/download/v#{version}/aps_linux_amd64.tar.gz"
-      sha256 "fb4d00d272025a20b155da29171f7dd7bdac7701ad640802a56902ca06e77adb"
+      sha256 "eb8b5e0d9c5ba75caf5e70dec05e6219f7efcd7682c915382da1a2c9b15fb059"
     end
   end
 
